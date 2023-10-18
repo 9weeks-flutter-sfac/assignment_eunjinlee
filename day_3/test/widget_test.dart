8d4a84547bdf87a240c02606ac48038fb37ab2b2
eunjinlee <luveunjin@gmail.com>
2023-10-18 13:08:26 +0900

    -

--- day_3/test/widget_test.dart
+++ day_3/test/widget_test.dart
@@ -7,7 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project_1/main.dart';
+
+import 'package:day_3/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
